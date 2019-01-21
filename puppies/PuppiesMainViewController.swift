789d949af8f10bc77a5f740cfce02010089d5410
Esmir Cabrera <esmircm@MacBook-Pro-de-Esmir.local>
2019-01-22 00:13:57 +0100

    adding icons and img on main screen

--- puppies/PuppiesMainViewController.swift
+++ puppies/PuppiesMainViewController.swift
@@ -15,6 +15,28 @@ class PuppiesMainViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myPuppieButton.layer.cornerRadius = 12
+        myPuppieButton.layer.borderWidth = 0
+        myPuppieButton.layer.transform = CATransform3DMakeScale(0.1,0.1,1)
+        UIButton.animate(withDuration: 0.7, animations: {
+            self.myPuppieButton.layer.transform = CATransform3DMakeScale(1.25,1.25,1)
+        },completion: { finished in
+            UIButton.animate(withDuration: 0.5, animations: {
+                self.myPuppieButton.layer.transform = CATransform3DMakeScale(1,1,1)
+            })
+        })
+        
+        puppiesButton.layer.cornerRadius = 12
+        puppiesButton.layer.borderWidth = 0
+        puppiesButton.layer.transform = CATransform3DMakeScale(0.1,0.1,1)
+        UIButton.animate(withDuration: 0.5, animations: {
+            self.puppiesButton.layer.transform = CATransform3DMakeScale(1.25,1.25,1)
+        },completion: { finished in
+            UIButton.animate(withDuration: 0.3, animations: {
+                self.puppiesButton.layer.transform = CATransform3DMakeScale(1,1,1)
+            })
+        })
     
     }
 }
